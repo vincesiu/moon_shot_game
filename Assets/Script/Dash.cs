e0dbac77a37a912c1f8741c5047c325053641716
rsmitsuda <rsmitsuda@gmail.com>
2020-11-02 21:43:42 -0800

    added dash and basic movement scripts

---
+++ Assets/Script/Dash.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour {
+    private DashState state = DashState.Ready;
+    private float dashTime;
+    public float dashDistance = 5f;
+    private Rigidbody2D myRigidBody;
+
+    public float dashSpeed;
+    public float startDashTime;
+
+
+    public Vector3 speedTemp;
+
+    bool keyPressed = false;
+
+
+    void calculateVelocity() {
+        float tempX = transform.position.x +  5 * Time.deltaTime;
+        float tempY = transform.position.y + 5 * Time.deltaTime;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
+        //myRigidBody.velocity = new Vector3(5, 5);
+    }
+
+    // Update is called once per frame
+    void Update() {
+ 
+        switch (state) {
+            case DashState.Ready:
+                if (dashTime > 0) {
+                    //Debug.Log("the key is pressed: " + myRigidBody.velocity);
+                    if (keyPressed) {
+                        //keep updating the veloctiy of the dude
+                        if (Input.GetKey(KeyCode.LeftArrow)) {
+                            myRigidBody.velocity = Vector2.left * dashSpeed;
+
+                            Debug.Log("the key is pressed: left");
+                            Debug.Log("the key is pressed: " + myRigidBody.velocity);
+
+                        } else if (Input.GetKey(KeyCode.RightArrow)) {
+                            myRigidBody.velocity = Vector2.right * dashSpeed;
+
+                            Debug.Log("the key is pressed: right");
+                            Debug.Log("the key is pressed: " + myRigidBody.velocity);
+
+                        } else if (Input.GetKey(KeyCode.UpArrow)) {
+                            myRigidBody.velocity = Vector2.up * dashSpeed;
+
+                            Debug.Log("the key is pressed: up");
+                            Debug.Log("the key is pressed: " + myRigidBody.velocity);
+
+                        } else if (Input.GetKey(KeyCode.DownArrow)) {
+                            //myRigidBody.velocity = new Vector3(myRigidBody.velocity.x * 10f, myRigidBody.velocity.y * 10f);
+                            myRigidBody.velocity = Vector2.down * dashSpeed;
+
+                            Debug.Log("the key is pressed: down");
+                            Debug.Log("the key is pressed: " + myRigidBody.velocity);
+
+                        } else {
+                            Debug.Log("NO KEY IS BEING PRESSED");
+                        }
+
+                        //reduce this until it's 0, then set the state to cooldown
+                        dashTime -= Time.deltaTime;
+
+                    } else {
+                        //if space is not pressed, check if space key is pressed
+                        keyPressed = Input.GetKeyDown(KeyCode.Space);
+                    }
+                } else {
+                    state = DashState.Cooldown;
+                }
+                break;
+            case DashState.Cooldown:
+                //dashTime -= Time.deltaTime;
+               
+
+                if (dashTime <= 0) {
+                    //reset the state of the dude
+                    dashTime = startDashTime;
+                    myRigidBody.velocity = new Vector3(0, 0);
+                    keyPressed = false;
+                    state = DashState.Ready;
+                    Debug.Log("reverting state back to ready");
+                }
+                
+                break;
+
+        }
+
+        //SPRINT STUFF
+    /*
+        switch (state) {
+            case DashState.Ready:
+                bool keyPressed = Input.GetKeyDown(KeyCode.Space);
+                Debug.Log("the key is pressed: " + keyPressed);
+                if (keyPressed) {
+                    speedTemp = myRigidBody.velocity;
+
+                    //multiply the velocity by 3
+                    myRigidBody.velocity = new Vector3(myRigidBody.velocity.x * 10f, myRigidBody.velocity.y * 10f);
+
+                    //set the state to dashing
+                    state = DashState.Dashing;
+                    Debug.Log("DASHING");
+
+                }
+                break;
+            case DashState.Dashing:
+                dashTime += Time.deltaTime * 4;
+                if (dashTime >= dashDistance) {
+                    dashTime += Time.deltaTime * 3;
+                    state = DashState.Cooldown;
+                    
+                    //set speed back to the saved speed
+                    myRigidBody.velocity = speedTemp;
+
+                }
+                break;
+            case DashState.Cooldown:
+                dashTime -= Time.deltaTime;
+
+                if (dashTime <= 0) {
+                    dashTime = 0;
+                    state = DashState.Ready;
+                }
+                break;
+
+        }    */
+
+    }
+
+    
+    public enum DashState {
+        Ready,
+        Dashing,
+        Cooldown
+    }
+
+}
---
+++ Assets/Script/DudeMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeMovement : MonoBehaviour { 
+    public float velocity;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+
+    void MoveDude() {
+        myRigidBody.MovePosition(
+            transform.position + change * velocity * Time.deltaTime
+            );
+
+        //Debug.Log(velocity);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        //Debug.Log(change);
+
+        if (change != Vector3.zero && (myRigidBody.velocity == Vector2.zero)) {
+            MoveDude();
+
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+        } else {
+            Debug.Log("THE VELOCITY IS: " + myRigidBody.velocity);
+
+        }
+    }
+}
