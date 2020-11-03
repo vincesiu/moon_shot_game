7f67185192ada73ed59d0fa983068cd2147936c9
rsmitsuda <rsmitsuda@gmail.com>
2020-11-02 23:44:35 -0800

    added movement to dude. it's really bad right now. pls fix me

--- Assets/Script/DudeMovement.cs
+++ Assets/Script/DudeMovement.cs
@@ -6,8 +6,10 @@ public class DudeMovement : MonoBehaviour {
     public float velocity;
     private Rigidbody2D myRigidBody;
     private Vector3 change;
-    private Animator animator;
+    public Animator animator;
+    private Vector2 movement;
 
+    
     void MoveDude() {
         myRigidBody.MovePosition(
             transform.position + change * velocity * Time.deltaTime
@@ -24,6 +26,7 @@ public class DudeMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
@@ -32,11 +35,20 @@ public class DudeMovement : MonoBehaviour {
         if (change != Vector3.zero && (myRigidBody.velocity == Vector2.zero)) {
             MoveDude();
 
-            animator.SetFloat("moveX", change.x);
-            animator.SetFloat("moveY", change.y);
+            animator.SetFloat("Horizontal", change.x);
+            animator.SetFloat("Vertical", change.y);
+            animator.SetFloat("Speed", change.sqrMagnitude);
         } else {
             Debug.Log("THE VELOCITY IS: " + myRigidBody.velocity);
 
         }
+        
+        /*
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        */
     }
 }
