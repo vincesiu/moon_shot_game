77adc0a07f58327b773ada52a3c0ffbc90295616
rsmitsuda <rsmitsuda@gmail.com>
2020-11-03 22:19:06 -0800

    camera follow. player weapon not showing up right now

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+    }
+}
---
+++ Assets/Script/PlayerAim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerAim : MonoBehaviour {
+
+    private Transform aimTransform;
+
+    private void Awake() {
+        aimTransform = transform.Find("Aim");
+    }
+
+    public static Vector3 GetMouseWorldPosition() {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ() {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera) {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera) {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 mousePosition = GetMouseWorldPosition();
+        aimTransform.LookAt(mousePosition);
+        Debug.Log("we are in the player aim function");
+
+    }
+}
