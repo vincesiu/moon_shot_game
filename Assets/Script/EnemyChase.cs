87da6a8f0a0e7ba56f02ba0f1447f1ab292c2b7a
nathan <nwwong16@gmail.com>
2020-11-03 20:59:47 -0800

    Enemy Chase Script
    added enemy place holder sprite and enemy chase scripts

---
+++ Assets/Script/EnemyChase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour
+{
+    public float speed;
+
+    public Transform target;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+    }
+}
