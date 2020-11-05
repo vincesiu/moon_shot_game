00efc2195f0163362ff0c8ce7a27fed600ca3189
vince <vince.siu4@gmail.com>
2020-11-04 18:31:54 -0800

    Add Title Screen
    * Start button switches to scene at idx 1
    * End button does not do anything yet
    * Basic examples of how to add text and iamges

---
+++ Assets/TitleMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    public void onExitClick()
+    {
+        Debug.Log("Exit button clicked");
+    }
+
+    public void onStartClick(int targetScene)
+    {
+        Debug.Log("Start button clicked");
+        Debug.Log(string.Format("Switching to scene {0}", targetScene));
+        SceneManager.LoadScene(targetScene);
+    }
+}
