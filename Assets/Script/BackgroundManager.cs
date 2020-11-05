4cde78145849b825d224ce3dd8723349530b328c
vince <vince.siu4@gmail.com>
2020-11-04 20:48:39 -0800

    Add pause overlay

---
+++ Assets/Script/BackgroundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape pressed");
+            SceneManager.LoadSceneAsync("PauseOverlay", LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ Assets/Script/PauseOverlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseOverlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            Debug.Log("meow");
+        }
+        */
+    }
+
+    public void ClickButtonExit()
+    {
+        Debug.Log("Exit button clicked from Pause Overlay");
+        Application.Quit();
+    }
+
+    public void ClickButtonReturn()
+    {
+        Debug.Log("Return button clicked from the Pause Overlay");
+        Unload();
+    }
+
+    void Unload()
+    {
+        SceneManager.UnloadSceneAsync("PauseOverlay");
+    }
+}
