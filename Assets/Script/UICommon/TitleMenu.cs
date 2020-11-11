6d0a50cedf7a7a8898f6259b4d74a7b2dfa45d3a
Vince <vince.siu4@gmail.com>
2020-11-10 20:32:10 -0800

    Add outro to the title screen

---
+++ Assets/Script/UICommon/TitleMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleMenu : MonoBehaviour
+{
+    bool loadingGame;
+
+    public void onExitClick()
+    {
+        Debug.Log("Exit button clicked");
+    }
+
+    public void onStartClick(int targetScene)
+    {
+        Debug.Log("Start button clicked");
+        Debug.Log(string.Format("Switching to scene {0}", targetScene));
+        GameObject startButton = GameObject.Find("StartButton");
+        Button btn = startButton.GetComponent<Button>();
+        btn.interactable = false;
+        StartCoroutine(StartGame(targetScene));
+
+    }
+
+    IEnumerator StartGame(int targetScene)
+    {
+        AsyncOperation handle = SceneManager.LoadSceneAsync(targetScene);
+        handle.allowSceneActivation = false;
+        yield return new WaitForSeconds(2);
+        handle.allowSceneActivation = true;
+
+    }
+}
