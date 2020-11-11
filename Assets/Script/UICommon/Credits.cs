6688cbc7a3526625950d9e794667e4978c47c90d
Vince <vince.siu4@gmail.com>
2020-11-10 20:46:54 -0800

    Add ability to go from credits scene back to title scene

---
+++ Assets/Script/UICommon/Credits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCard : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
